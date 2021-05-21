aa0a3a8cb1c92e029916b8f673b280c2475ae55f
Evgeny Kyselgov <kyselgov@gmail.com>
2021-05-21 19:30:35 +0300

    add filters

---
+++ WeaponAlmanac/WeaponAlmanac/Data Model/Filters/CollectorFilter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponAlmanac.Data_Model.Filters
+{
+    public class CollectorFilter : IDataModelCollectorFilter
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public bool? HasRareWeapon { get; set; } = null;
+
+        public bool Pass(Collector collector)
+        {
+            bool res = true;
+
+            if (res && !string.IsNullOrEmpty(Name))
+            {
+                res = collector.Name.Contains(Name, StringComparison.OrdinalIgnoreCase);
+            }
+
+            if (res && !string.IsNullOrEmpty(Country))
+            {
+                res = collector.Country.Contains(Country, StringComparison.OrdinalIgnoreCase);
+            }
+
+            if (res && HasRareWeapon.HasValue)
+            {
+                res = collector.RareIds.Count>0 == HasRareWeapon.Value;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ WeaponAlmanac/WeaponAlmanac/Data Model/Filters/WeaponFilter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponAlmanac.Data_Model.Filters
+{
+    public class WeaponFilter : IDataModelWeaponFilter
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Country { get; set; }
+        public int ManufactureStartDate { get; set; } = DataModelUtils.InvalidDate;
+        public int ManufactureEndDate { get; set; } = DataModelUtils.InvalidDate;
+
+        public bool Pass(Weapon weapon)
+        {
+            bool res = true;
+
+            if (res && !string.IsNullOrEmpty(Name))
+            {
+                res = weapon.Name.Contains(Name, StringComparison.OrdinalIgnoreCase);
+            }
+
+            if (res && !string.IsNullOrEmpty(Description))
+            {
+                res = weapon.Description.Contains(Description, StringComparison.OrdinalIgnoreCase);
+            }
+
+            if (res && !string.IsNullOrEmpty(Country))
+            {
+                res = weapon.Country.Contains(Country, StringComparison.OrdinalIgnoreCase);
+            }
+
+            if (res && (ManufactureStartDate != DataModelUtils.InvalidDate) &&
+                       (ManufactureEndDate != DataModelUtils.InvalidDate))
+            {
+                res = weapon.ManufactureDate >= ManufactureStartDate &&
+                      weapon.ManufactureDate <= ManufactureEndDate;
+            }
+
+            return res;
+        }
+    }
+}
