912a747e0fe86bb6c404ebf50347d29303453d18
Ritok-K <marharyta.kiselhova@nure.ua>
2021-11-29 14:14:04 +0200

    fix .gitignore; add missed User data

---
+++ MovieStore/MovieStore/Data/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStore.Data
+{
+    enum UserRole
+    {
+        User,
+        Manager,
+    }
+
+    class User
+    {
+        internal int Id { get; set; }
+        internal string FirstName { get; set; }
+        internal string SecondName { get; set; }
+        internal string EMail { get; set; }
+        internal UserRole Role { get; set; }
+        internal string PasswordHash { get; set; }
+        internal string Salt { get; init; } = Utility.Seсurity.GenerateSalt();
+
+        internal void SetPassword(string password)
+        {
+            var hash = Utility.Seсurity.GetSHA256Hash($"{password}{Salt}");
+            PasswordHash = hash;
+        }
+
+        internal bool CheckPassword(string password)
+        {
+            var hash = Utility.Seсurity.GetSHA256Hash($"{password}{Salt}");
+
+            var res = PasswordHash.Equals(hash, StringComparison.OrdinalIgnoreCase);
+            return res;
+        }
+    }
+}
