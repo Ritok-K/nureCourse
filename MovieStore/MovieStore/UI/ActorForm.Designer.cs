e1cee3787362561dc6ecd66426f58b66079b8344
Ritok-K <marharyta.kiselhova@nure.ua>
2021-12-02 00:32:18 +0200

    add Actor Form

--- MovieStore/MovieStore/UI/ActorForm.Designer.cs
+++ MovieStore/MovieStore/UI/ActorForm.Designer.cs
@@ -29,13 +29,159 @@ namespace MovieStore.UI
         /// </summary>
         private void InitializeComponent()
         {
+            this.m_firstNameLabel = new System.Windows.Forms.Label();
+            this.m_firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.m_secondNameLabel = new System.Windows.Forms.Label();
+            this.m_secondNameTextBox = new System.Windows.Forms.TextBox();
+            this.m_birthDateLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.m_familyStatusLabel = new System.Windows.Forms.Label();
+            this.m_familyStatusComboBox = new System.Windows.Forms.ComboBox();
+            this.m_countryLabel = new System.Windows.Forms.Label();
+            this.m_countryTextBox = new System.Windows.Forms.TextBox();
+            this.m_awardsDescriptionLabel = new System.Windows.Forms.Label();
+            this.m_awardsDescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.m_okButton = new System.Windows.Forms.Button();
+            this.m_cancelButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // m_firstNameLabel
+            // 
+            this.m_firstNameLabel.AutoSize = true;
+            this.m_firstNameLabel.Location = new System.Drawing.Point(29, 29);
+            this.m_firstNameLabel.Name = "m_firstNameLabel";
+            this.m_firstNameLabel.Size = new System.Drawing.Size(80, 20);
+            this.m_firstNameLabel.TabIndex = 1;
+            this.m_firstNameLabel.Text = "First name:";
+            // 
+            // m_firstNameTextBox
+            // 
+            this.m_firstNameTextBox.Location = new System.Drawing.Point(153, 26);
+            this.m_firstNameTextBox.Name = "m_firstNameTextBox";
+            this.m_firstNameTextBox.Size = new System.Drawing.Size(447, 27);
+            this.m_firstNameTextBox.TabIndex = 2;
+            // 
+            // m_secondNameLabel
+            // 
+            this.m_secondNameLabel.AutoSize = true;
+            this.m_secondNameLabel.Location = new System.Drawing.Point(29, 90);
+            this.m_secondNameLabel.Name = "m_secondNameLabel";
+            this.m_secondNameLabel.Size = new System.Drawing.Size(102, 20);
+            this.m_secondNameLabel.TabIndex = 3;
+            this.m_secondNameLabel.Text = "Second name:";
+            // 
+            // m_secondNameTextBox
+            // 
+            this.m_secondNameTextBox.Location = new System.Drawing.Point(153, 87);
+            this.m_secondNameTextBox.Name = "m_secondNameTextBox";
+            this.m_secondNameTextBox.Size = new System.Drawing.Size(447, 27);
+            this.m_secondNameTextBox.TabIndex = 4;
+            // 
+            // m_birthDateLabel
+            // 
+            this.m_birthDateLabel.AutoSize = true;
+            this.m_birthDateLabel.Location = new System.Drawing.Point(29, 157);
+            this.m_birthDateLabel.Name = "m_birthDateLabel";
+            this.m_birthDateLabel.Size = new System.Drawing.Size(77, 20);
+            this.m_birthDateLabel.TabIndex = 5;
+            this.m_birthDateLabel.Text = "Birth date:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(153, 154);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 27);
+            this.textBox1.TabIndex = 6;
+            // 
+            // m_familyStatusLabel
+            // 
+            this.m_familyStatusLabel.AutoSize = true;
+            this.m_familyStatusLabel.Location = new System.Drawing.Point(349, 157);
+            this.m_familyStatusLabel.Name = "m_familyStatusLabel";
+            this.m_familyStatusLabel.Size = new System.Drawing.Size(96, 20);
+            this.m_familyStatusLabel.TabIndex = 7;
+            this.m_familyStatusLabel.Text = "Family status:";
+            // 
+            // m_familyStatusComboBox
+            // 
+            this.m_familyStatusComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.m_familyStatusComboBox.FormattingEnabled = true;
+            this.m_familyStatusComboBox.Location = new System.Drawing.Point(467, 154);
+            this.m_familyStatusComboBox.Name = "m_familyStatusComboBox";
+            this.m_familyStatusComboBox.Size = new System.Drawing.Size(133, 28);
+            this.m_familyStatusComboBox.TabIndex = 8;
+            // 
+            // m_countryLabel
+            // 
+            this.m_countryLabel.AutoSize = true;
+            this.m_countryLabel.Location = new System.Drawing.Point(29, 219);
+            this.m_countryLabel.Name = "m_countryLabel";
+            this.m_countryLabel.Size = new System.Drawing.Size(63, 20);
+            this.m_countryLabel.TabIndex = 9;
+            this.m_countryLabel.Text = "Country:";
+            // 
+            // m_countryTextBox
+            // 
+            this.m_countryTextBox.Location = new System.Drawing.Point(153, 219);
+            this.m_countryTextBox.Name = "m_countryTextBox";
+            this.m_countryTextBox.Size = new System.Drawing.Size(447, 27);
+            this.m_countryTextBox.TabIndex = 10;
+            // 
+            // m_awardsDescriptionLabel
+            // 
+            this.m_awardsDescriptionLabel.AutoSize = true;
+            this.m_awardsDescriptionLabel.Location = new System.Drawing.Point(29, 279);
+            this.m_awardsDescriptionLabel.Name = "m_awardsDescriptionLabel";
+            this.m_awardsDescriptionLabel.Size = new System.Drawing.Size(139, 20);
+            this.m_awardsDescriptionLabel.TabIndex = 11;
+            this.m_awardsDescriptionLabel.Text = "Awards description:";
+            // 
+            // m_awardsDescriptionTextBox
+            // 
+            this.m_awardsDescriptionTextBox.Location = new System.Drawing.Point(211, 276);
+            this.m_awardsDescriptionTextBox.Multiline = true;
+            this.m_awardsDescriptionTextBox.Name = "m_awardsDescriptionTextBox";
+            this.m_awardsDescriptionTextBox.Size = new System.Drawing.Size(389, 96);
+            this.m_awardsDescriptionTextBox.TabIndex = 12;
+            // 
+            // m_okButton
+            // 
+            this.m_okButton.Location = new System.Drawing.Point(322, 402);
+            this.m_okButton.Name = "m_okButton";
+            this.m_okButton.Size = new System.Drawing.Size(144, 29);
+            this.m_okButton.TabIndex = 13;
+            this.m_okButton.Text = "Ok";
+            this.m_okButton.UseVisualStyleBackColor = true;
+            // 
+            // m_cancelButton
+            // 
+            this.m_cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.m_cancelButton.Location = new System.Drawing.Point(487, 403);
+            this.m_cancelButton.Name = "m_cancelButton";
+            this.m_cancelButton.Size = new System.Drawing.Size(108, 29);
+            this.m_cancelButton.TabIndex = 14;
+            this.m_cancelButton.Text = "Cancel";
+            this.m_cancelButton.UseVisualStyleBackColor = true;
             // 
             // ActorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(637, 456);
+            this.Controls.Add(this.m_cancelButton);
+            this.Controls.Add(this.m_okButton);
+            this.Controls.Add(this.m_awardsDescriptionTextBox);
+            this.Controls.Add(this.m_awardsDescriptionLabel);
+            this.Controls.Add(this.m_countryTextBox);
+            this.Controls.Add(this.m_countryLabel);
+            this.Controls.Add(this.m_familyStatusComboBox);
+            this.Controls.Add(this.m_familyStatusLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.m_birthDateLabel);
+            this.Controls.Add(this.m_secondNameTextBox);
+            this.Controls.Add(this.m_secondNameLabel);
+            this.Controls.Add(this.m_firstNameTextBox);
+            this.Controls.Add(this.m_firstNameLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -43,9 +189,25 @@ namespace MovieStore.UI
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Actor";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label m_firstNameLabel;
+        private System.Windows.Forms.TextBox m_firstNameTextBox;
+        private System.Windows.Forms.Label m_secondNameLabel;
+        private System.Windows.Forms.TextBox m_secondNameTextBox;
+        private System.Windows.Forms.Label m_birthDateLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label m_familyStatusLabel;
+        private System.Windows.Forms.ComboBox m_familyStatusComboBox;
+        private System.Windows.Forms.Label m_countryLabel;
+        private System.Windows.Forms.TextBox m_countryTextBox;
+        private System.Windows.Forms.Label m_awardsDescriptionLabel;
+        private System.Windows.Forms.TextBox m_awardsDescriptionTextBox;
+        private System.Windows.Forms.Button m_okButton;
+        private System.Windows.Forms.Button m_cancelButton;
     }
 }
--- MovieStore/MovieStore/UI/ActorForm.cs
+++ MovieStore/MovieStore/UI/ActorForm.cs
@@ -10,11 +10,73 @@ using System.Windows.Forms;
 
 namespace MovieStore.UI
 {
+    enum ActorFormMode
+    {
+        NewActor,
+        EditActor,
+        ViewActor,
+    }
+
     public partial class ActorForm : Form
     {
-        public ActorForm()
+        ActorFormMode Mode { get; set; } = ActorFormMode.NewActor;
+        Data.Actor Actor { get; set; } = new Data.Actor();
+
+        public ActorForm() 
         {
             InitializeComponent();
         }
+
+        internal void SetMode(ActorFormMode mode, Data.Actor actor)
+        {
+            Mode = mode;
+
+            if (mode == ActorFormMode.EditActor ||
+                mode == ActorFormMode.ViewActor)
+            {
+                Actor = actor;
+            }
+        }
+
+        //void InitControls()
+        //{
+        //    var studio = Program.DB.GetStudio();
+        //    m_studioComboBox.DataSource = studio;
+        //    m_studioComboBox.DisplayMember = nameof(Data.Studio.Title);
+        //    m_studioComboBox.ValueMember = nameof(Data.Studio.Id);
+
+        //    UpdateControls();
+        //    UpdateData(false);
+        //}
+
+        void UpdateControls()
+        {
+            switch (Mode)
+            {
+                case ActorFormMode.NewActor:
+                    Text = "Add new actor";
+                    m_okButton.Text = "Add actor";
+                    break;
+                case ActorFormMode.EditActor:
+                    Text = "Update actor";
+                    m_okButton.Text = "Update actor";
+                    break;
+                case ActorFormMode.ViewActor:
+                    Text = "Actor";
+                    m_okButton.Text = "Ok";
+                    break;
+            }
+
+            //var isViewMode = Mode == MovieFormMode.ViewMovie;
+            //m_studioComboBox.Enabled = !isViewMode;
+            //m_titleTextBox.ReadOnly = isViewMode;
+            //m_countryTextBox.ReadOnly = isViewMode;
+            //m_yearTextBox.ReadOnly = isViewMode;
+            //m_descriptionTextBox.ReadOnly = isViewMode;
+            //m_genreTextBox.ReadOnly = isViewMode;
+            //m_priceTextBox.ReadOnly = isViewMode;
+            //m_imdbTextBox.ReadOnly = isViewMode;
+        }
+
     }
 }
